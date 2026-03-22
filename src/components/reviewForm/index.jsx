278d2ee4ee4e61e44cf37da46ffc9cdd8ff4d297
lekan2003 <94309955+lekan2003@users.noreply.github.com>
2026-03-22 20:12:08 +0000

    Stored reviews in context

--- src/components/reviewForm/index.jsx
+++ src/components/reviewForm/index.jsx
@@ -1,32 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { useForm, Controller } from "react-hook-form";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { useNavigate } from "react-router";
+
+import { MoviesContext } from "../../contexts/moviesContext";
 
 const ratings = [
-  {
-    value: 5,
-    label: "Excellent",
-  },
-  {
-    value: 4,
-    label: "Good",
-  },
-  {
-    value: 3,
-    label: "Average",
-  },
-  {
-    value: 2,
-    label: "Poor",
-  },
-  {
-    value: 0,
-    label: "Terrible",
-  },
+  { value: 5, label: "Excellent" },
+  { value: 4, label: "Good" },
+  { value: 3, label: "Average" },
+  { value: 2, label: "Poor" },
+  { value: 0, label: "Terrible" },
 ];
 
 const styles = {
@@ -42,9 +32,6 @@ const styles = {
       marginTop: 2,
     },
   },
-  textField: {
-    width: "40ch",
-  },
   submit: {
     marginRight: 2,
   },
@@ -57,30 +44,40 @@ const styles = {
 };
 
 const ReviewForm = ({ movie }) => {
+  const [open, setOpen] = useState(false);
   const [rating, setRating] = useState(3);
-  
+
+  const navigate = useNavigate();
+  const context = useContext(MoviesContext);
+
   const defaultValues = {
     author: "",
     review: "",
     agree: false,
     rating: "3",
   };
-  
+
   const {
     control,
     formState: { errors },
     handleSubmit,
     reset,
-  } = useForm(defaultValues);
+  } = useForm({ defaultValues });
 
   const handleRatingChange = (event) => {
     setRating(event.target.value);
   };
 
+  const handleSnackClose = () => {
+    setOpen(false);
+    navigate("/movies/favorites");
+  };
+
   const onSubmit = (review) => {
     review.movieId = movie.id;
     review.rating = rating;
-    console.log(review);
+    context.addReview(movie, review);
+    setOpen(true);
   };
 
   return (
@@ -89,32 +86,41 @@ const ReviewForm = ({ movie }) => {
         Write a review
       </Typography>
 
-      <form sx={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
+      <Snackbar
+        sx={styles.snack}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={open}
+        onClose={handleSnackClose}
+      >
+        <MuiAlert severity="success" variant="filled" onClose={handleSnackClose}>
+          <Typography variant="h6">
+            Thank you for submitting a review
+          </Typography>
+        </MuiAlert>
+      </Snackbar>
+
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <Controller
           name="author"
           control={control}
           rules={{ required: "Name is required" }}
-          defaultValue=""
-          render={({ field: { onChange, value } }) => (
+          render={({ field }) => (
             <TextField
               sx={{ width: "40ch" }}
               variant="outlined"
               margin="normal"
               required
-              onChange={onChange}
-              value={value}
-              id="author"
               label="Author's name"
-              name="author"
               autoFocus
+              {...field}
             />
           )}
         />
+
         {errors.author && (
-          <Typography variant="h6" component="p">
-            {errors.author.message}
-          </Typography>
+          <Typography variant="h6">{errors.author.message}</Typography>
         )}
+
         <Controller
           name="review"
           control={control}
@@ -122,40 +128,36 @@ const ReviewForm = ({ movie }) => {
             required: "Review cannot be empty.",
             minLength: { value: 10, message: "Review is too short" },
           }}
-          defaultValue=""
-          render={({ field: { onChange, value } }) => (
+          render={({ field }) => (
             <TextField
               variant="outlined"
               margin="normal"
               required
               fullWidth
-              name="review"
-              value={value}
-              onChange={onChange}
-              label="Review text"
-              id="review"
               multiline
               minRows={10}
+              label="Review text"
+              {...field}
             />
           )}
         />
+
         {errors.review && (
-          <Typography variant="h6" component="p">
-            {errors.review.message}
-          </Typography>
+          <Typography variant="h6">{errors.review.message}</Typography>
         )}
 
         <Controller
-          control={control}
           name="rating"
-          render={({ field: { onChange, value } }) => (
+          control={control}
+          render={({ field: { onChange } }) => (
             <TextField
-              id="select-rating"
               select
-              variant="outlined"
               label="Rating Select"
               value={rating}
-              onChange={handleRatingChange}
+              onChange={(e) => {
+                handleRatingChange(e);
+                onChange(e);
+              }}
               helperText="Don't forget your rating"
             >
               {ratings.map((option) => (
@@ -167,26 +169,21 @@ const ReviewForm = ({ movie }) => {
           )}
         />
 
-        <Box sx={styles.buttons}>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            sx={styles.submit}
-          >
+        <Box>
+          <Button type="submit" variant="contained" sx={styles.submit}>
             Submit
           </Button>
+
           <Button
             type="reset"
             variant="contained"
             color="secondary"
-            sx={styles.submit}
-            onClick={() => {
+            onClick={() =>
               reset({
                 author: "",
-                content: "",
-              });
-            }}
+                review: "",
+              })
+            }
           >
             Reset
           </Button>
