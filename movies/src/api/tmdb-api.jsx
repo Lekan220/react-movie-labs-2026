6f2f6742d4280a66c6d18e9137d54dae27916e09
lekan2003 <94309955+lekan2003@users.noreply.github.com>
2026-03-25 12:49:23 +0000

    Assignment 01 - 2026 done

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -1,139 +1,83 @@
-export const getMovies = () => {
-  return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-  ).then((response) => {
-    if (!response.ok) {
-      return response.json().then((error) => {
-        throw new Error(error.status_message || "Something went wrong");
-      });
-    }
-    return response.json();
-  })
-  .catch((error) => {
-      throw error
-  });
-};
+const apiKey = import.meta.env.VITE_TMDB_KEY;
+const baseUrl = "https://api.themoviedb.org/3";
 
-
-export const getMovie = (args) => {
-  //console.log(args)
-  const [, idPart] = args.queryKey;
-  const { id } = idPart;
-  return fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
-  ).then((response) => {
-    if (!response.ok) {
-      return response.json().then((error) => {
-        throw new Error(error.status_message || "Something went wrong");
-      });
-    }
-    return response.json();
-  })
-  .catch((error) => {
-    throw error
- });
-};
-
-  export const getGenres = () => {
-    return fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
-        import.meta.env.VITE_TMDB_KEY +
-        "&language=en-US"
-    ).then( (response) => {
-      if (!response.ok) {
-        return response.json().then((error) => {
-          throw new Error(error.status_message || "Something went wrong");
-        });
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      throw error
-   });
-  };
-
-
-  export const getMovieImages = ({ queryKey }) => {
-    const [, idPart] = queryKey;
-    const { id } = idPart;
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    ).then( (response) => {
-      if (!response.ok) {
-        return response.json().then((error) => {
-          throw new Error(error.status_message || "Something went wrong");
-        });
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      throw error
-   });
-  };
-
-
-  export const getMovieReviews = ({ queryKey }) => {
-    const [, idPart] = queryKey;
-    const { id } = idPart;
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    ).then( (response) => {
-      if (!response.ok) {
-        return response.json().then((error) => {
-          throw new Error(error.status_message || "Something went wrong");
-        });
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      throw error
-   });
-  };
-// Fetches the list of trending movies for the day from TMDB API
-export const getTrendingMovies = () => {
-  return fetch(
-    `https://api.themoviedb.org/3/trending/movie/day?api_key=${import.meta.env.VITE_TMDB_KEY}`
-  ).then((response) => {
-    if (!response.ok) {
-      return response.json().then((error) => {
-        throw new Error(error.status_message || "Something went wrong");
-      });
-    }
-    return response.json();
-  })
-  .catch((error) => {
-    throw error;
-  });
-};
-
-export const getUpcomingMovies = async () => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
-  );
-
+// Helper to handle responses
+const handleResponse = async (response) => {
   if (!response.ok) {
-    throw new Error("Failed to fetch upcoming movies");
+    const error = await response.json();
+    throw new Error(error.status_message || "Something went wrong");
   }
-
   return response.json();
 };
 
-export const getMovieRecommendations = ({ queryKey }) => {
-  const [, idPart] = queryKey;
-  const { id } = idPart;
+// Get all movies
+export const getMovies = async () => {
+  const response = await fetch(
+    `${baseUrl}/discover/movie?api_key=${apiKey}&language=en-US&include_adult=false&include_video=false&page=1`
+  );
+  return handleResponse(response);
+};
 
-  return fetch(
-    `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        return response.json().then((error) => {
-          throw new Error(error.status_message || "Something went wrong");
-        });
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      throw error;
-    });
+// Get single movie
+export const getMovie = async ({ queryKey }) => {
+  const [, { id }] = queryKey;
+
+  const response = await fetch(
+    `${baseUrl}/movie/${id}?api_key=${apiKey}`
+  );
+  return handleResponse(response);
+};
+
+// Get genres
+export const getGenres = async () => {
+  const response = await fetch(
+    `${baseUrl}/genre/movie/list?api_key=${apiKey}&language=en-US`
+  );
+  return handleResponse(response);
+};
+
+// Get movie images
+export const getMovieImages = async ({ queryKey }) => {
+  const [, { id }] = queryKey;
+
+  const response = await fetch(
+    `${baseUrl}/movie/${id}/images?api_key=${apiKey}`
+  );
+  return handleResponse(response);
+};
+
+// Get movie reviews
+export const getMovieReviews = async ({ queryKey }) => {
+  const [, { id }] = queryKey;
+
+  const response = await fetch(
+    `${baseUrl}/movie/${id}/reviews?api_key=${apiKey}`
+  );
+  return handleResponse(response);
+};
+
+// Trending movies
+export const getTrendingMovies = async () => {
+  const response = await fetch(
+    `${baseUrl}/trending/movie/day?api_key=${apiKey}`
+  );
+  return handleResponse(response);
+};
+
+// Upcoming movies
+export const getUpcomingMovies = async () => {
+  const response = await fetch(
+    `${baseUrl}/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`
+  );
+  return handleResponse(response);
+};
+
+// Recommendations
+export const getMovieRecommendations = async ({ queryKey }) => {
+  const [, { id }] = queryKey;
+
+  const response = await fetch(
+    `${baseUrl}/movie/${id}/recommendations?api_key=${apiKey}&language=en-US&page=1`
+  );
+  return handleResponse(response);
 };
